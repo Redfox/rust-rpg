508dec2474b9626db553986b9e7bff87470524e9
Redfox <vitor_pereira.2013@hotmail.com>
2021-03-12 19:48:08 -0300

    feat: player movement

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use crate::player::Player;
+use crate::player::{ Player, Direction };
 use sdl2::pixels::Color;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
@@ -51,9 +51,11 @@ fn main() -> Result<(), String> {
   let texture_creator = canvas.texture_creator();
   let texture = texture_creator.load_texture("assets/bardo.png")?;
 
-  let player = Player {
+  let mut player = Player {
     position: Point::new(0, 0),
     sprite: Rect::new(0, 0, 26, 36),
+    speed: 0,
+    direction: Direction::Right,
   };
 
   let mut event_pump = sdl_context.event_pump()?;
@@ -66,16 +68,35 @@ fn main() -> Result<(), String> {
         Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
           println!("quiting...");
           break 'running;
-        }
+        },
+        Event::KeyDown { keycode: Some(Keycode::A), repeat: false, .. } => {
+          player.move_to(Direction::Left);
+        },
+        Event::KeyDown { keycode: Some(Keycode::D), repeat: false, .. } => {
+          player.move_to(Direction::Right);
+        },
+        Event::KeyDown { keycode: Some(Keycode::W), repeat: false, .. } => {
+          player.move_to(Direction::Up);
+        },
+        Event::KeyDown { keycode: Some(Keycode::S), repeat: false, .. } => {
+          player.move_to(Direction::Down);
+        },
+        Event::KeyUp { keycode: Some(Keycode::A), repeat: false, .. } |
+        Event::KeyUp { keycode: Some(Keycode::D), repeat: false, .. } |
+        Event::KeyUp { keycode: Some(Keycode::W), repeat: false, .. } |
+        Event::KeyUp { keycode: Some(Keycode::S), repeat: false, .. }  => {
+          player.stop();
+        },
         _ => {}
       }
     }
 
     i = (i + 1) % 255;
+    player.update();
 
     render(&mut canvas, Color::RGB(i, 64, 255 - i), &texture, &player)?;
     
-    ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
+    ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 20));
   }
 
   Ok(())
--- src/player.rs
+++ src/player.rs
@@ -1,7 +1,48 @@
 use sdl2::rect::{ Point, Rect };
 
+const PLAYER_MOVEMENT_SPEED: i32 = 20;
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum Direction {
+  Up,
+  Down,
+  Left,
+  Right,
+}
+
 #[derive(Debug)]
 pub struct Player {
   pub position: Point,
   pub sprite: Rect,
+  pub speed: i32,
+  pub direction: Direction,
+}
+
+impl Player {
+  pub fn update(&mut self) {
+    use self::Direction::*;
+    match &self.direction {
+      Left => {
+        self.position = self.position.offset(-self.speed, 0);
+      },
+      Right => {
+        self.position = self.position.offset(self.speed, 0);
+      },
+      Up => {
+        self.position = self.position.offset(0, -self.speed);
+      },
+      Down => {
+        self.position = self.position.offset(0, self.speed);
+      }
+    }
+  }
+
+  pub fn move_to(&mut self, direction: Direction) {
+    self.speed = PLAYER_MOVEMENT_SPEED;
+    self.direction = direction;
+  }
+
+  pub fn stop(&mut self) {
+    self.speed = 0;
+  }
 }
