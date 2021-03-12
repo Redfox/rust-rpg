3fd74827583a20cc5f8f3f17db2997063a85ad01
Redfox <vitor_pereira.2013@hotmail.com>
2021-03-12 18:51:00 -0300

    feat: render image

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,21 @@
 use sdl2::pixels::Color;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
-use sdl2::render::WindowCanvas;
 use std::time::Duration;
+use sdl2::render::{ WindowCanvas, Texture };
+use sdl2::image::{self, LoadTexture, InitFlag };
 
-fn render(canvas: &mut WindowCanvas, color: Color) {
+
+
+fn render(canvas: &mut WindowCanvas, color: Color, texture: &Texture) -> Result<(), String> {
   canvas.set_draw_color(color);
   canvas.clear();
+
+  canvas.copy(texture, None, None)?;
+
   canvas.present();
+
+  Ok(())
 }
 
 fn main() -> Result<(), String> {
@@ -15,15 +23,21 @@ fn main() -> Result<(), String> {
   let sdl_context = sdl2::init()?;
   let video_subsystem = sdl_context.video()?;
 
-  let window = video_subsystem.window("Game", 800, 600)
+  let _image_context = image::init(InitFlag::PNG | InitFlag::JPG)?;
+
+  let window = video_subsystem
+      .window("Game", 800, 600)
       .position_centered()
       .build()
       .expect("could not initialize video subsystem");
 
   let mut canvas = window
-        .into_canvas()
-        .build()
-        .expect("could not make a canvas");
+      .into_canvas()
+      .build()
+      .expect("could not make a canvas");
+
+  let texture_creator = canvas.texture_creator();
+  let texture = texture_creator.load_texture("assets/bardo.png")?;
 
   let mut event_pump = sdl_context.event_pump()?;
   let mut i = 0;
@@ -42,7 +56,7 @@ fn main() -> Result<(), String> {
 
     i = (i + 1) % 255;
 
-    render(&mut canvas, Color::RGB(i, 64, 255 - i));
+    render(&mut canvas, Color::RGB(i, 64, 255 - i), &texture)?;
     
     ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
   }
